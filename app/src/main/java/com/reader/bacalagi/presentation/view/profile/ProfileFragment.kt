32ed6837aa0a640dd09ad7fcd79b2e04421c05fd
Tenshi-X <sevafarel17@gmail.com>
2024-06-10 00:06:33 +0700

    Add Transaction Menu on Profile Fragment

--- app/src/main/java/com/reader/bacalagi/presentation/view/profile/ProfileFragment.kt
+++ app/src/main/java/com/reader/bacalagi/presentation/view/profile/ProfileFragment.kt
@@ -1,12 +1,15 @@
 package com.reader.bacalagi.presentation.view.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import com.reader.bacalagi.R
 import com.reader.bacalagi.base.BaseFragment
 import com.reader.bacalagi.databinding.FragmentProfileBinding
+import com.reader.bacalagi.presentation.view.transaction.TransactionFragment
 
 class ProfileFragment : BaseFragment<FragmentProfileBinding>() {
 
@@ -19,7 +22,7 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>() {
     }
 
     override fun initAppBar() {
-        binding.mainToolbar.apply {
+        binding.mainToolbarProfile.apply {
             mainToolbar.title = "My Profile"
             mainToolbar.setNavigationIcon(R.drawable.ic_back)
 
@@ -30,6 +33,19 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>() {
     }
 
     override fun initUI() {
+        binding.itemListTransaction.apply {
+            tvSetting.text = "My Transaction"
+            ivItemSetting.setImageResource(R.drawable.ic_edit)
+
+//            root.setOnClickListener {
+//                val fragment = TransactionFragment()
+//                val fragmentManager = (context as AppCompatActivity).supportFragmentManager
+//                val fragmentTransaction = fragmentManager.beginTransaction()
+//                fragmentTransaction.replace(R.id.transactionFragment, fragment)
+//                fragmentTransaction.addToBackStack(null)
+//                fragmentTransaction.commit()
+//            }
+        }
         binding.itemEditProfile.apply {
             tvSetting.text = "Edit Profile"
             ivItemSetting.setImageResource(R.drawable.ic_edit)
