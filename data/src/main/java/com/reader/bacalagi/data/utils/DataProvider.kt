d4592516c681f602527dd6e6170397b0e63c75bf
Oki Firdaus Syah Putra <85375019+okifirsyah404@users.noreply.github.com>
2024-06-13 19:52:42 +0700

    Update DataProvider.kt

--- data/src/main/java/com/reader/bacalagi/data/utils/DataProvider.kt
+++ data/src/main/java/com/reader/bacalagi/data/utils/DataProvider.kt
@@ -5,7 +5,8 @@ import com.reader.bacalagi.data.BuildConfig
 object DataProvider {
     var databaseName: String = BuildConfig.DATABASE_NAME
 
-    //    var baseUrl: String = BuildConfig.BASE_URL
-    var baseUrl: String = "https://ddaa-118-99-121-162.ngrok-free.app/"
+    var baseUrl: String = BuildConfig.BASE_URL
+
+    //    var baseUrl: String = "https://ddaa-118-99-121-162.ngrok-free.app/"
     var areaBaseUrl: String = BuildConfig.AREA_BASE_URL
 }
