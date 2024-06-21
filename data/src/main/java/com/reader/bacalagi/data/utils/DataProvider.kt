2c7d1a500ebd6889d79d821c9759568e456663a9
Oki Firdaus Syah Putra <85375019+okifirsyah404@users.noreply.github.com>
2024-06-21 22:05:46 +0700

    Update DataProvider.kt

--- data/src/main/java/com/reader/bacalagi/data/utils/DataProvider.kt
+++ data/src/main/java/com/reader/bacalagi/data/utils/DataProvider.kt
@@ -4,10 +4,8 @@ import com.reader.bacalagi.data.BuildConfig
 
 object DataProvider {
     var databaseName: String = BuildConfig.DATABASE_NAME
-
-
-    var baseUrl: String = "https://api.readerlab.my.id/"
-//    var baseUrl: String = BuildConfig.BASE_URL
+    
+    var baseUrl: String = BuildConfig.BASE_URL
 
     var areaBaseUrl: String = BuildConfig.AREA_BASE_URL
 }
